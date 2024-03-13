890dfe38ed043e526f9e9351ccd9b89fb3d978b9
Tejasgatkal <gatkaltejas@gmail.com>
2024-03-13 18:45:41 +0530

    assignment_3

---
+++ assignment_3/que7.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(void)
+{
+ int num;
+
+ printf("enter the number\n");
+ scanf("%d",&num);
+
+ int i=1;
+
+ while(i != num)
+ {
+  if(num%i == 0)
+  {
+    if(i < num/i)
+  
+      printf("%d * %d = %d\n",i,num/i,num);
+  }
+   i++;
+  
+ }
+ 
+
+ return 0;
+}
